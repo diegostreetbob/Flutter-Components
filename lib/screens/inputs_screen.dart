61fcdfe9994c51f3c03a6bb3bd737145ec22216e
DiegoMGuillén <d761017@hotmail.com>
2022-06-10 18:30:45 +0200

    Update inputs_screen.dart
    primeros pasos en captura de datos de formulario

--- lib/screens/inputs_screen.dart
+++ lib/screens/inputs_screen.dart
@@ -8,6 +8,18 @@ class  InputsScreen extends StatelessWidget {
   //
   @override
   Widget build(BuildContext context) {
+      //Para tener acceso a los campos del formulario, en formulario y todo su estado se puede
+      //referenciar desde myFormKey
+      final GlobalKey<FormState> myFormKey = GlobalKey<FormState>();
+      //valores del formulario
+      final Map<String,String> formValues = {
+        "nombre":"Fernando",
+        "apellidos":"Lopez",
+        "email":"mail@mail.com",
+        "password":"123",
+        "roles":"Admin"
+      };
+      //
       return Scaffold(
       appBar: AppBar(
           title: const Text("Inputs & Forms")
@@ -16,20 +28,35 @@ class  InputsScreen extends StatelessWidget {
         //que sería para varios elementos.
       body: SingleChildScrollView(
           child: Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 20,vertical: 10
-            ),
-            child: Column(
-              children: const [
-                CustomInputField(hintText: "Nombre",suffixIcon: Icons.login,keyboardType: TextInputType.text),
-                SizedBox(height: 10),
-                CustomInputField(hintText: "Apellidos",suffixIcon: Icons.login,keyboardType: TextInputType.text),
-                SizedBox(height: 10),
-                CustomInputField(hintText: "Email",suffixIcon: Icons.mail_outline,keyboardType: TextInputType.emailAddress),
-                SizedBox(height: 10),
-                CustomInputField(hintText: "Password",suffixIcon: Icons.security_rounded,keyboardType: TextInputType.visiblePassword,obscureText: true),
-                SizedBox(height: 10)
-              ],
+            padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 10),
+            child: Form(//envuelve al widget que tiene los campos de formulario
+                key: myFormKey,
+                child: Column(
+                children: [
+                  const CustomInputField(hintText: "Nombre",suffixIcon: Icons.login,keyboardType: TextInputType.text),
+                  const SizedBox(height: 10),
+                  const CustomInputField(hintText: "Apellidos",suffixIcon: Icons.login,keyboardType: TextInputType.text),
+                  const SizedBox(height: 10),
+                  const CustomInputField(hintText: "Email",suffixIcon: Icons.mail_outline,keyboardType: TextInputType.emailAddress),
+                  const SizedBox(height: 10),
+                  const CustomInputField(hintText: "Password",suffixIcon: Icons.security_rounded,keyboardType: TextInputType.visiblePassword,obscureText: true),
+                  const SizedBox(height: 10),
+                  ElevatedButton(
+                      child: const Text("Guardar"),
+                      onPressed: (){
+                        //para minimizar el teclado cada vez que damos a guardar
+                        FocusScope.of(context).requestFocus(FocusNode());
+                        //aseguro no null
+                        if(myFormKey.currentState != null) {
+                          //
+                          if(!myFormKey.currentState!.validate()){
+                            print("formulario no válido");
+                          }
+                        }
+                      },
+                  )
+                ],
+              ),
             ),
           )
       ),
