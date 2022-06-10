7d6f08bb2cb5a53b9e2c69153256063b6383911a
DiegoMGuillén <d761017@hotmail.com>
2022-06-10 10:46:54 +0200

    Update inputs_screen.dart
    Se han tratado algunas características del de TextFormField

--- lib/screens/inputs_screen.dart
+++ lib/screens/inputs_screen.dart
@@ -5,8 +5,9 @@ import 'package:flutter/material.dart';
 import '/screens/screens.dart';
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 class  InputsScreen extends StatelessWidget {
+  //
   const InputsScreen ({Key? key}) : super(key: key);
-
+  //
   @override
   Widget build(BuildContext context) {
       return Scaffold(
@@ -30,7 +31,24 @@ class  InputsScreen extends StatelessWidget {
                   onChanged: (value){ //imprime lo escrito
                     print("value: $value");
                   },
-
+                  //validación
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  validator:(value){
+                    if(value != null) {
+                      return value.length < 4 ? "Mínimo escribir 4 caracteres" : "";
+                    }else{
+                      return "Este campo es requerido";
+                    }
+                  },
+                  decoration: const InputDecoration(
+                    hintText: "Nombre del usuario",
+                    labelText: "Nombre",
+                    helperText: "Introduzca un mínimo de 4 caracteres",
+                    suffixIcon: Icon(Icons.verified_user_outlined),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10))
+                    )
+                  ),
                 )
               ],
             ),
