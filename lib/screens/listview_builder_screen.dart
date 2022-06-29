8d7ca9606018d8a5c593d9119ecca551f44b8fac
DiegoMGuillén <d761017@hotmail.com>
2022-06-29 19:35:20 +0200

    Update listview_builder_screen.dart
    modificado para crear un indicador de carga centrado, y con la funcionalidad de que cuando termine de cargar suba todas las imágenes hacia arriba 120 píxeles de modo que se vea un poco de la primera de las imágenes cargadas.

--- lib/screens/listview_builder_screen.dart
+++ lib/screens/listview_builder_screen.dart
@@ -11,8 +11,9 @@ class ListViewBuilderScreen extends StatefulWidget {
 
 class _ListViewBuilderScreenState extends State<ListViewBuilderScreen> {
   //atributos
-  final List<int> imagesIds = [1,2,3,4,5,6,7,8,9,10];
+  final List<int> imagesIds = [1,2,3,4,5];
   final ScrollController scrollController = ScrollController();
+  bool isLoading = false;
   //creamos listeners
   @override
   void initState(){
@@ -20,13 +21,37 @@ class _ListViewBuilderScreenState extends State<ListViewBuilderScreen> {
     //
     scrollController.addListener(() {
       //posición actual y máxima posición posible
-      print("${scrollController.position.pixels},${scrollController.position.maxScrollExtent},${imagesIds.last},${imagesIds.length}");
+      //print("${scrollController.position.pixels},${scrollController.position.maxScrollExtent},${imagesIds.last},${imagesIds.length}");
       //si se acerca al final llamamos a la operación de cargar mas elementos
       if((scrollController.position.pixels + 500)>(scrollController.position.maxScrollExtent)){
-        loadItems();
+
+        fetchData();
       }
     });
   }
+
+  //método asíncrono para simular una situación real de carga de imágenes donde suelen haber
+  //retardos
+  Future fetchData () async {
+    //si ya está cargando no hacer nada
+    if(isLoading) return;
+    isLoading= true;
+    setState((){});
+    //simulamos el retardo
+    await Future.delayed(const Duration(seconds: 3));
+    //llamamos
+    loadItems();
+    isLoading=false;
+    setState((){});
+    //cuando termina de cargar sube todas las imágenes hacia arriba 120 pixeles
+    scrollController.animateTo(
+        scrollController.position.pixels+120,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.fastOutSlowIn
+    );
+
+  }
+
 
   void loadItems(){
     final lastId = imagesIds.last;
@@ -40,26 +65,60 @@ class _ListViewBuilderScreenState extends State<ListViewBuilderScreen> {
 
   @override
   Widget build(BuildContext context) {
+      //atributos
+      final size = MediaQuery.of(context).size;//tamaño de la pantalla actual
       return Scaffold(
         body: MediaQuery.removePadding(
           context: context,
           removeTop: true,//quita espacio en blanco superior
           removeBottom: true,//quita espacion en blanco inferior
-          child: ListView.builder(
-              controller: scrollController,
-              itemCount: imagesIds.length,
-              itemBuilder:(BuildContext context, int index){
-                return FadeInImage(
-                    width: double.infinity, //evita saltos por reajuste de tamaño al cargar
-                    height: 300, //evita saltos por reajuste de tamaño al cargar
-                    fit: BoxFit.cover,//quita franja blanca entre imágenes
-                    placeholder: const AssetImage("resources/images/jar-loading.gif"),
-                    image: NetworkImage('https://picsum.photos/500/300?image=${imagesIds[index]}')
-                    );
-              },
+          child: Stack(
+            children: [
+              ListView.builder(
+                  controller: scrollController,
+                  itemCount: imagesIds.length,
+                  itemBuilder:(BuildContext context, int index){
+                    return FadeInImage(
+                        width: double.infinity, //evita saltos por reajuste de tamaño al cargar
+                        height: 300, //evita saltos por reajuste de tamaño al cargar
+                        fit: BoxFit.cover,//quita franja blanca entre imágenes
+                        placeholder: const AssetImage("resources/images/jar-loading.gif"),
+                        image: NetworkImage('https://picsum.photos/500/300?image=${imagesIds[index]}')
+                        );
+                  },
+              ),
+              //para centrar elemento dentro del Stack
+              if(isLoading)//solo lo muestra si está cargando
+              Positioned(
+                  bottom: 20,
+                  //al centro de la pantalla
+                  left: size.width * 0.5 - 30,
+                  child: const _LoadingIcon()
+              )
+            ],
           ),
         ),
         );
+  }
+}
+
+class _LoadingIcon extends StatelessWidget {
+  const _LoadingIcon({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.white.withOpacity(0.5),
+        borderRadius: BorderRadius.circular(100)
+      ),
+      width: 60,
+      height: 60,
+      child: const CircularProgressIndicator(),
+    );
   }
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////
