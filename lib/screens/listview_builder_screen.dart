ba8428cb379b2a1743e7d379dfffb6834aa16074
DiegoMGuillén <d761017@hotmail.com>
2022-06-29 18:41:03 +0200

    Update listview_builder_screen.dart
    Se ha realizado una modificación para que una vez cargados los 10 primeros elementos se carguen más de 5 en 5 conforme se va haciendo scroll

--- lib/screens/listview_builder_screen.dart
+++ lib/screens/listview_builder_screen.dart
@@ -2,8 +2,41 @@ import 'package:flutter/material.dart';
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-class ListViewBuilderScreen extends StatelessWidget {
+class ListViewBuilderScreen extends StatefulWidget {
   const ListViewBuilderScreen ({Key? key}) : super(key: key);
+
+  @override
+  State<ListViewBuilderScreen> createState() => _ListViewBuilderScreenState();
+}
+
+class _ListViewBuilderScreenState extends State<ListViewBuilderScreen> {
+  //atributos
+  final List<int> imagesIds = [1,2,3,4,5,6,7,8,9,10];
+  final ScrollController scrollController = ScrollController();
+  //creamos listeners
+  @override
+  void initState(){
+    super.initState();
+    //
+    scrollController.addListener(() {
+      //posición actual y máxima posición posible
+      print("${scrollController.position.pixels},${scrollController.position.maxScrollExtent},${imagesIds.last},${imagesIds.length}");
+      //si se acerca al final llamamos a la operación de cargar mas elementos
+      if((scrollController.position.pixels + 500)>(scrollController.position.maxScrollExtent)){
+        loadItems();
+      }
+    });
+  }
+
+  void loadItems(){
+    final lastId = imagesIds.last;
+    //añade 5 elementos a la lista,
+    imagesIds.addAll(
+        [1,2,3,4,5].map((e) => lastId+e)
+    );
+    setState((){});//redibujamos
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -13,14 +46,15 @@ class ListViewBuilderScreen extends StatelessWidget {
           removeTop: true,//quita espacio en blanco superior
           removeBottom: true,//quita espacion en blanco inferior
           child: ListView.builder(
-              itemCount: 10,
+              controller: scrollController,
+              itemCount: imagesIds.length,
               itemBuilder:(BuildContext context, int index){
                 return FadeInImage(
                     width: double.infinity, //evita saltos por reajuste de tamaño al cargar
                     height: 300, //evita saltos por reajuste de tamaño al cargar
                     fit: BoxFit.cover,//quita franja blanca entre imágenes
                     placeholder: const AssetImage("resources/images/jar-loading.gif"),
-                    image: NetworkImage('https://picsum.photos/500/300?image=${index + 1}')
+                    image: NetworkImage('https://picsum.photos/500/300?image=${imagesIds[index]}')
                     );
               },
           ),
